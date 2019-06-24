ef4d051aed00e76d12d0d311143c4ec100b887b4
BrittX <BrittX@users.noreply.github.com>
2019-06-23 20:32:14 -0400

    Using an example docker-compose file that uses postgres for kong db and konga for the ui. Includes location for a wait-for-it script that could/would be used for autodiscovery

---
+++ scripts/wait-for-kong.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# wait-for-kong.sh
+
+# Initially check to see if the API Gateway is up
+
+# When it's up, check if service has been registered
+  # Service not registered, register it with kong and then register routes
